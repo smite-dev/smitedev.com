edce26f0b11b16b0a63e9b2ceb809f8dc986b45c
smite <kjsmith415@gmail.com>
2024-07-13 21:42:28 -0400

    changed pesto to smite

--- src/components/CheemsText.jsx
+++ src/components/CheemsText.jsx
@@ -7,7 +7,7 @@ function CheemsText() {
       <div className="App-header">
         <img src={cheems} className="cheems-logo" alt="CHEEMS" />
         <div className="pesto">
-            PESTO    
+            SMITE    
         </div>
       </div>
     </div>
